f63bd52d82329678b0e0ca6d0f4e28c84d6a6d10
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-01-04 19:51:19 +0530

    shorten implementation: beta

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,20 +1,39 @@
 import axios from "axios";
 
-async function getShortURL() {
-  const API_key = "#";
-  const URLtoShorten = "#";
-  try {
-    const rawData = await  axios({
-        method: "POST",
-        url:
-          "https://cors-anywhere.herokuapp.com/https://kutt.it/api/url/submit",
-        headers: { 'X-API-Key': API_key },
-        data: { target: URLtoShorten }
-      }
-    );
-    console.log(rawData.data.shortUrl);
-  } catch (error) {
-    console.log(error);
-  }
-}
-getShortURL();
+async function getShortURL(URLtoShorten) {
+    let shortLink;
+    const API_key = "????????????????????????????";
+    console.log(URLtoShorten);
+    try {
+        const rawData = await axios({
+            method: "POST",
+            url:
+            "https://kutt.it/api/url/submit",
+            headers: { 
+                'X-API-Key': API_key 
+            },
+            data: { target: URLtoShorten }
+        });
+        shortLink = rawData.data.shortUrl;
+        console.log(shortLink);
+    } catch (error) {
+        console.log(error);
+    }
+    return shortLink;
+};
+
+chrome.runtime.onMessage.addListener(
+    (request, sender, sendResponse) => {
+        let shortLink;
+
+        if(request.msg == "start") {
+            getShortURL(request.pageUrl).then((data) => {
+                shortLink = data;
+                console.log("short url = "+ shortLink);
+            });
+            console.log(`return link ${shortLink}`);
+            sendResponse({ shortUrl: `${shortLink}` });
+            // sendResponse({ shortUrl: `${shortLink}` });
+        }
+    }
+);
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,6 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById('text');
-    link.addEventListener('click', function() {
-        link.textContent = "Testing";
+document.addEventListener('DOMContentLoaded', () => {
+    
+    let longUrl;
+
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, (tabs) => {
+       longUrl = tabs[0].url;
+       chrome.runtime.sendMessage({ msg: "start", pageUrl: `${longUrl}` }, (response) => {
+           console.log(response);
+           console.log(response.shortUrl);  
+        });
     });
+
+
+    // let link = document.getElementById('text');
+    // link.addEventListener('click', function() {
+    //     console.log(bgpage);
+    //     link.textContent = longUrl;
+    // });
 });
