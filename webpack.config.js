d4b6559efa10400335ad8df70b81ebfcaa1b5d2f
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-01-14 18:19:50 +0530

    ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -8,131 +8,131 @@ const ZipPlugin = require('zip-webpack-plugin');
 
 module.exports = () => ({
     entry: {
-      options: ["./src/scripts/options.js", './src/styles/options.scss'],
-      popup: ["./src/scripts/popup.js", './src/styles/popup.scss'],
-      background: ["./src/scripts/background.js"]
+        options: ['./src/scripts/options.js', './src/styles/options.scss'],
+        popup: ['./src/scripts/popup.js', './src/styles/popup.scss'],
+        background: ['./src/scripts/background.js']
     },
     output: {
-      path: path.resolve(__dirname, "extension", process.env.TARGET),
-      filename: "js/[name].js",
-      publicPath: ""
+        path: path.resolve(__dirname, 'extension', process.env.TARGET),
+        filename: 'js/[name].js',
+        publicPath: ''
     },
     node: {
         fs: 'empty'
     },
     module: {
         rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader'
-          },  
-          {
-            test: /\.(html)$/,
-            use: {
-              loader: "html-loader",
-              options: {
-                attrs: [':data-src']
-              }
-            }
-          },
-          {
-            test: /\.svg$/,
-            loader: 'url-loader'
-          },
-          {
-            test: /\.scss$/,
-            use: [
-                {
-                    loader: 'file-loader',
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader',
                     options: {
-                        name: '[name].css',
-                        context: './src/styles/',
-                        outputPath: 'css/',
-                        publicPath: '../'
+                        attrs: [':data-src']
                     }
-                },
-                {
-                    loader: 'extract-loader'
-                },
-                {
-                    loader: 'css-loader',
-                    options: {
-                        sourceMap: true
-                    }
-                },
-                {
-                    loader: 'resolve-url-loader'
-                },
-                {
-                    loader: 'postcss-loader',
-                    options: {
-                      plugins: function () {
-                        return [
-                          require('precss'),
-                          require('autoprefixer')
-                        ];
-                      }
-                    }
-                },
-                {
-                    loader: 'sass-loader',
                 }
-              ]
-            }  
+            },
+            {
+                test: /\.svg$/,
+                loader: 'url-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].css',
+                            context: './src/styles/',
+                            outputPath: 'css/',
+                            publicPath: '../'
+                        }
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'resolve-url-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('precss'),
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ]
+            }
         ]
     },
     plugins: [
-      new CleanWebpackPlugin([
-        `extension/${process.env.TARGET}`,
-        `extension/${process.env.TARGET}.zip`
-      ]),
-      new CopyWebpackPlugin([ 
-          { 
-            from: 'src/assets', 
-            to: 'assets' 
-          },
-          {
-            from: `src/manifest.${process.env.TARGET}.json`,
-            to: 'manifest.json'
-          }
-      ]),
-      new HtmlWebpackPlugin({
-        template: 'src/options.html',
-        inject: false,
-        filename: 'options.html'
-      }),
-      new HtmlWebpackPlugin({
-        template: 'src/popup.html',
-        inject: false,
-        filename: 'popup.html'
-      })
+        new CleanWebpackPlugin([
+            `extension/${process.env.TARGET}`,
+            `extension/${process.env.TARGET}.zip`
+        ]),
+        new CopyWebpackPlugin([
+            {
+                from: 'src/assets',
+                to: 'assets'
+            },
+            {
+                from: `src/manifest.${process.env.TARGET}.json`,
+                to: 'manifest.json'
+            }
+        ]),
+        new HtmlWebpackPlugin({
+            template: 'src/options.html',
+            inject: false,
+            filename: 'options.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/popup.html',
+            inject: false,
+            filename: 'popup.html'
+        })
     ],
     optimization: {
-      minimizer: [
-        new OptimizeCssAssetsPlugin({
-          assetNameRegExp: /\.css$/g,
-          cssProcessor: require('cssnano'),
-          cssProcessorOptions: { 
-            map: false
-          },
-          cssProcessorPluginOptions: {
-            preset: ['default', { discardComments: { removeAll: true } }],
-          },
-          canPrint: true
-        }),
-        new TerserPlugin({
-          cache: true,
-          parallel: true
-        }),
-        new ZipPlugin({
-          path: path.resolve(__dirname, "extension"),
-          filename: `${process.env.TARGET}.zip`
-        })        
-      ]
+        minimizer: [
+            new OptimizeCssAssetsPlugin({
+                assetNameRegExp: /\.css$/g,
+                cssProcessor: require('cssnano'),
+                cssProcessorOptions: {
+                    map: false
+                },
+                cssProcessorPluginOptions: {
+                    preset: ['default', { discardComments: { removeAll: true } }],
+                },
+                canPrint: true
+            }),
+            new TerserPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new ZipPlugin({
+                path: path.resolve(__dirname, 'extension'),
+                filename: `${process.env.TARGET}.zip`
+            })
+        ]
     },
     devServer: {
         port: 3000,
-        contentBase: "./extensions"
+        contentBase: './extensions'
     }
 });
