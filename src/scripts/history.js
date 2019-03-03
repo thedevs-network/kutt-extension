3e851262c594bb2892d9f66e0c3c3acf552667b5
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-03-03 10:38:18 +0530

    bling.js

--- src/scripts/history.js
+++ src/scripts/history.js
@@ -1,13 +1,13 @@
 import browser from 'webextension-polyfill';
+import { $, $$ } from './bling';
 import QRCode from 'qrcode';
 
 
-const clear__btn = document.getElementById('table__clearAll--btn'),
-    main__element = document.getElementById('delegation__element');
+const clear__btn = '#table__clearAll--btn',
+    main__element = '#delegation__element';
 
 
-// on page load
-document.addEventListener('DOMContentLoaded', () => {
+document.on('DOMContentLoaded', () => {
     let updatedHTML;
     const html = '<tr class="table__body--holder" id="table__body-%num%"><td class="table__body--original"><a href="%longLink%" class="table__body--originalURL" target="_blank" rel="noopener">%longLink%</a></td><td class="table__body--shortened" id="table__shortened-%num%"><div class="table__body--shortenBody"><a href="%shortLink%" id="shortUrl-%num%" class="table__body--shortenURL" target="_blank" rel="noopener">%shortLink%</a></div></td><td class="table__body--functionBtns"><div class="table__body--btnHolder" id="btns-%num%"><button type="button" class="table__body--copy" id="copy-%num%" title="Copy"><img class="selectDisable icon__img" src="assets/copy.svg" alt="copy" /></button><button type="button" class="table__body--qrcode" id="qrcode-%num%" title="QR Code"><img class="selectDisable icon__img" src="assets/qrcode.svg" alt="QR Code" /></button></div></td></tr>';
     // get longURL, shortURL
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         updatedHTML = updatedHTML.replace(/%num%/g, ++pass);
                         updatedHTML = updatedHTML.replace(/%shortLink%/g, el.shortUrl);
                         // inject to DOM
-                        main__element.insertAdjacentHTML('afterbegin', updatedHTML);
+                        $(main__element).insertAdjacentHTML('afterbegin', updatedHTML);
                     }
                 }
             } else {
@@ -37,24 +37,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 // Clear all history
-clear__btn.addEventListener('click', () => {
-    browser.storage.local.set({ URL_array: [] }).then(() => {
-        main__element.parentNode.removeChild(main__element);
+$(clear__btn).on('click', () => {
+    browser.storage.local.set({
+        URL_array: []
+    }).then(() => {
+        $(main__element).parentNode.removeChild($(main__element));
     });
 });
 
 
+// Buttons Function
 function buttonAction(type, id) {
     function flashCopy(flashHTML) {
-        document.getElementById(`table__shortened-${id}`).insertAdjacentHTML('afterbegin', flashHTML);
+        $(`#table__shortened-${id}`).insertAdjacentHTML('afterbegin', flashHTML);
         setTimeout(() => {
-            const copy__alert = document.getElementById('flash_copy');
-            copy__alert.parentNode.removeChild(copy__alert);
+            $('#flash_copy').parentNode.removeChild($('#flash_copy'));
         }, 1300);
     }
     if (type === 'copy') {
         // 1. get url
-        const shortLink = document.getElementById(`shortUrl-${id}`).textContent;
+        const shortLink = $(`#shortUrl-${id}`).textContent;
         // 2, add to clipboard
         try {
             const copyTextarea = shortLink;
@@ -76,24 +78,23 @@ function buttonAction(type, id) {
         let updatedHTML;
         const html = '<div class="table__qrcodePopup--div" id="qrcode__template"><div class="table__qrcode--popup"><div class="table__qrcode--holder"><img id="table__qrcode" src="%qrcodeLink%" alt="QRCode" /></div><div class="table__closebtn--holder"><button type="button" class="table__closebtn--inner" id="close__btn-%num%">Close</button></div></div></div>';
         // 1. get short link
-        const shortUrl = document.getElementById(`shortUrl-${id}`).textContent;
+        const shortUrl = $(`#shortUrl-${id}`).textContent;
         // 2. generate qrcode
         QRCode.toDataURL(shortUrl)
             .then(qrcodeURL => {
                 // 3. display popup menu with link
                 updatedHTML = html.replace('%qrcodeLink%', qrcodeURL);
                 updatedHTML = updatedHTML.replace('%num%', id);
-                document.getElementById(`btns-${id}`).insertAdjacentHTML('afterend', updatedHTML);
+                $(`#btns-${id}`).insertAdjacentHTML('afterend', updatedHTML);
             })
             .catch(err => {
                 // fetch qrcode from http://goqr.me
                 const qrcode__api = 'https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=';
                 updatedHTML = html.replace('%qrcodeLink%', `${qrcode__api}${shortUrl}`);
-                document.getElementById(`btns-${id}`).insertAdjacentHTML('afterend', updatedHTML);
+                $(`#btns-${id}`).insertAdjacentHTML('afterend', updatedHTML);
             });
     } else if (type === 'close__btn') {
-        const qrcode__child = document.getElementById('qrcode__template');
-        qrcode__child.parentNode.removeChild(qrcode__child);
+        $('#qrcode__template').parentNode.removeChild($('#qrcode__template'));
     }
 }
 
@@ -112,11 +113,11 @@ function getButtonDetails(e) {
 
 
 // Button Action (qrcode / copy)
-main__element.addEventListener('click', getButtonDetails);
+$(main__element).on('click', getButtonDetails);
 
 
 // prevent enter key press
-document.addEventListener('keypress', (e) => {
+document.on('keypress', (e) => {
     const keyCode = e.which || e.keyCode;
     // enter key
     if (keyCode === 13) {
--- src/scripts/options.js
+++ src/scripts/options.js
@@ -1,51 +1,49 @@
+import { $, $$ } from './bling';
 import browser from 'webextension-polyfill';
 
 
-const pwd__holder = document.getElementById('pwd__holder'),
-    submit__btn = document.getElementById('button__submit'),
-    pwd__value = document.getElementById('password--value'),
-    api__holder = document.getElementById('api__key--value'),
-    pwd__view = document.getElementById('view__password--eye'),
-    pwd__switch = document.getElementById('password__label--switch'),
-    pwd__checkbox = document.getElementById('password__label--checkbox'),
-    history__checkbox = document.getElementById('history__label--checkbox'),
-    autocopy__checkbox = document.getElementById('autocopy__label--checkbox');
+const pwd__holder = '#pwd__holder',
+    submit__btn = '#button__submit',
+    pwd__value = '#password--value',
+    api__holder = '#api__key--value',
+    pwd__eye = '#view__password--eye',
+    pwd__switch = '#password__label--switch',
+    pwd__checkbox = '#password__label--checkbox',
+    history__checkbox = '#history__label--checkbox',
+    autocopy__checkbox = '#autocopy__label--checkbox';
 
 
-// update UI - API Key on options page load
-document.addEventListener('DOMContentLoaded', () => {
-    // replace the input value with current value on load
+document.on('DOMContentLoaded', () => {
     browser.storage.local.get(['key', 'pwd', 'userOptions'])
         .then(result => {
             // to string
             const API_KEY = `${result.key}`;
             let pwd = result.pwd;
             if (API_KEY === 'undefined') {
-                api__holder.value = '';
+                $(api__holder).value = '';
             } else {
-                api__holder.value = API_KEY;
-                pwd__checkbox.checked = result.userOptions.pwdForUrls;
+                $(api__holder).value = API_KEY;
+                $(pwd__checkbox).checked = result.userOptions.pwdForUrls;
                 // if disabled -> delete save password
                 if (!result.userOptions.pwdForUrls) {
                     pwd = '';
                 }
-                pwd__value.value = pwd;
-                // view password holder
+                $(pwd__value).value = pwd;
                 toggleView(result.userOptions.pwdForUrls);
             }
-            autocopy__checkbox.checked = result.userOptions.autoCopy;
-            history__checkbox.checked = result.userOptions.keepHistory;
+            $(autocopy__checkbox).checked = result.userOptions.autoCopy;
+            $(history__checkbox).checked = result.userOptions.keepHistory;
         });
 });
 
 
-// Store Data and alert message
+// Store Data and Alert message
 const saveData = () => {
-    let password = pwd__value.value;
-    const API_KEY = api__holder.value;
-    let pwdForUrls = pwd__checkbox.checked;
-    const autoCopy = autocopy__checkbox.checked;
-    const keepHistory = history__checkbox.checked;
+    let password = $(pwd__value).value;
+    const API_KEY = $(api__holder).value;
+    let pwdForUrls = $(pwd__checkbox).checked;
+    const autoCopy = $(autocopy__checkbox).checked;
+    const keepHistory = $(history__checkbox).checked;
 
     if (password == '') {
         pwdForUrls = false;
@@ -67,10 +65,9 @@ const saveData = () => {
         URL_array: [],
         userOptions: userOptions
     }).then(() => {
-        // Saved Alert
-        submit__btn.textContent = 'Saved!';
+        $(submit__btn).textContent = 'Saved';
         setTimeout(() => {
-            submit__btn.textContent = 'Save';
+            $(submit__btn).textContent = 'Save';
             // close current tab
             browser.tabs.getCurrent().then((tabInfo) => {
                 browser.tabs.remove(tabInfo.id);
@@ -79,13 +76,10 @@ const saveData = () => {
     });
 };
 
+// Save Data
+$(submit__btn).on('click', saveData);
 
-// on save button click
-submit__btn.addEventListener('click', saveData);
-
-
-// on enter key press
-document.addEventListener('keypress', (e) => {
+document.on('keypress', (e) => {
     if (e.keyCode === 13) {
         saveData();
     }
@@ -93,30 +87,30 @@ document.addEventListener('keypress', (e) => {
 
 
 // Show Password
-pwd__view.addEventListener('click', () => {
-    const element = pwd__value;
+$(pwd__eye).on('click', () => {
+    const element = $(pwd__value);
     if (element.type === 'password') {
         element.type = 'text';
-        pwd__view.textContent = 'HIDE';
+        $(pwd__eye).textContent = 'HIDE';
     } else {
         element.type = 'password';
-        pwd__view.textContent = 'SHOW';
+        $(pwd__eye).textContent = 'SHOW';
     }
 });
 
 
-// Password Holder View Toggle
+// Toggle Password Holder View
 function toggleView(checked) {
     if (checked) {
-        pwd__holder.classList.remove('d-none');
+        $(pwd__holder).classList.remove('d-none');
     } else {
-        pwd__holder.classList.add('d-none');
+        $(pwd__holder).classList.add('d-none');
     }
 }
 
 
-// Password Option toggle key press
-pwd__switch.addEventListener('click', () => {
-    const checked = pwd__checkbox.checked;
+// Password Enable/Disable Switch
+$(pwd__switch).on('click', () => {
+    const checked = $(pwd__checkbox).checked;
     toggleView(checked);
 });
