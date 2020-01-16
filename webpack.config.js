af5b08bee8d022d04a60c2533e0aa24027de8283
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-01-16 12:15:28 +0530

    feat: set up webpack configuration for ts

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const { CheckerPlugin } = require('awesome-typescript-loader')
+
+const sourcePath = path.join(__dirname, 'src');
+const destPath = path.join(__dirname, 'dist');
+const nodeEnv = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  watch: nodeEnv === 'watch',
+  entry: {
+    background: path.join(sourcePath, 'background', 'index.ts');
+    options: path.join(sourcePath, 'options', 'index.ts');
+    popup: path.join(sourcePath, 'popup', 'index.ts');
+  },
+  output: {
+    path: destPath;
+    filename: '[name].bundle.js';
+  },
+  module: {
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    rules: [
+      {
+        test: /\.(js|ts|tsx)?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new CheckerPlugin();
+  ]
+}
